006e0e11e856c8aa6a8a92676b5ec4bd63be753a
Quentin Gilleron <quentin.gilleron5627@gmail.com>
2025-01-07 16:48:44 +0100

    Mise à jour du test photoCompare pour naviguer vers Google, cliquer sur le bouton de refus et vérifier la capture d'écran de la page d'accueil.

--- tests/photoCompare.spec.ts
+++ tests/photoCompare.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from '@playwright/test';
 
+
 test('Has a title', async ({ page }) => {
-    await page.goto('https://google.com');
-    await expect(page.title()).resolves.toMatch('Google');
+    await page.goto('https://www.google.com/');
+    await page.getByRole('button', { name: 'Tout refuser' }).click();
+    await page.getByRole('img', { name: 'Google' }).click();
+    await expect(page).toHaveScreenshot('landing.png');
 });
