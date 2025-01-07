b09269cb59502efa92428e9be8fa451390d8bf74
Quentin Gilleron <quentin.gilleron5627@gmail.com>
2025-01-07 10:22:53 +0100

    Amélioration de la méthode openSearchAndClick pour inclure des vérifications d'URL, de visibilité et d'éditabilité des éléments, ainsi que la gestion des bannières de cookies.

--- page_objects/landing.page.ts
+++ page_objects/landing.page.ts
@@ -5,21 +5,53 @@ export class LandingPage {
         this.page = page;
     }
     async openSearchAndClick() {
-        // Open the landing page
+        
+        //  Wait for the page to load
+        await this.page.waitForURL('**', { waitUntil: 'load' });
         await this.page.goto('/');
 
-        // Vérify if the cookie banner is displayed and close it
-        await this.page.getByRole('button', { name: 'Tout refuser et fermer' }).waitFor({ state: 'visible' });
-        await this.page.getByRole('button', { name: 'Tout refuser et fermer' }).click();
 
+        // Check if the URL is the expected one
+        const currentURL = this.page.url();
+        if (currentURL !== 'https://www.decathlon.fr/') {
+            throw new Error(`Unexpected URL: ${currentURL}`);
+        }
+
+        // Close the cookies banner
+        if (await this.page.getByRole('button', { name: 'Tout refuser et fermer' }).isVisible()) {
+            await this.page.getByRole('button', { name: 'Tout refuser et fermer' }).click();
+        }
+        else if (await this.page.getByRole('button', { name: 'Accepter & Fermer: Accepter' }).isVisible()) {
+            await this.page.getByRole('button', { name: 'Accepter & Fermer: Accepter' }).click();
+        }
+        
         // Vérify if the search bar is displayed and click on it
-        await this.page.getByPlaceholder('Rechercher un produit, un').click();
-        await this.page.getByPlaceholder('Rechercher un produit, un').isEditable();
+        if (await this.page.getByPlaceholder('Rechercher un produit, un').isVisible()) {
+            await this.page.getByPlaceholder('Rechercher un produit, un').click();
+        } else {
+            throw new Error('Search bar is not visible');
+        }
 
         // Vérify if the search bar is editable and fill it
-        await this.page.getByPlaceholder('Rechercher un produit, un').fill('bonnet');
+        if (await this.page.getByPlaceholder('Rechercher un produit, un').isEditable()) {
+            await this.page.getByPlaceholder('Rechercher un produit, un').fill('bonnet');
+        } else {
+            throw new Error('Search bar is not editable');
+        }
 
-        // Submit the search
-        await this.page.getByRole('button', { name: '' }).click();
+        // Vérify if the search button is displayed and click on it
+        const searchButton = this.page.getByRole('button', { name: '' });
+        if (await searchButton.isVisible()) {
+            if (await searchButton.isEnabled()) {
+                await searchButton.click();
+            } else {
+                throw new Error('Search button is not clickable');
+            }
+        } else {
+            throw new Error('Search button is not visible');
+        }
+
+
+
     } 
 }
--- tests/pom.spec.ts
+++ tests/pom.spec.ts
@@ -1,8 +1,9 @@
-import { test } from '@playwright/test';
+import { test, Browser } from '@playwright/test';
 import { LandingPage } from '../page_objects/landing.page.ts';
 import { SearchProductPage } from '../page_objects/search_product.page.ts';
 import { Product } from '../page_objects/product.page.ts';
 import { Cart } from '../page_objects/cart.page.ts';
+
 
 test('User can find products by searching and add to cart', async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 });
@@ -18,3 +19,21 @@ test('User can find products by searching and add to cart', async ({ page }) => 
     await product.clickOnAddToCart();
     await cartPage.clickOnCartPage();
 });
+
+// // l'utilisateur ne peux pas compiler ses panier si il ouvre deux pages en même temps
+// test('User can not compile his cart if he opens two pages at the same time', async ({ page, browser }) => {
+//     const context = await browser.newContext();
+//     const page2 = await context.newPage();
+//     await page2.setViewportSize({ width: 1920, height: 1080 });
+    
+//     const landingPage = new LandingPage(page);
+//     const searchProductPage = new SearchProductPage(page);
+//     const product = new Product(page);
+//     const cartPage = new Cart(page);
+
+//     await landingPage.openSearchAndClick();
+//     await searchProductPage.clickOnProduct();
+//     await product.clickOnAddToCart();
+//     await cartPage.clickOnCartPage();
+//     await cartPage.clickOnCartPage();
+// });
